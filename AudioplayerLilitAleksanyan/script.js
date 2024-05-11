ebbb69446f3986a05b007b0c845736cda44f6b35
LilitAleksanyan <lilit.aleksanayn1@tumo.org>
2024-05-11 13:28:53 +0400

    data

---
+++ AudioplayerLilitAleksanyan/script.js
@@ -0,0 +1,22 @@
+let data = {
+    title : [
+        "If you go",
+        "Last chance to love",
+        "Hotel Rendezvous",
+        "Daisy",
+        "Nare"
+    ],
+    song : [
+        "music/Nemra - If you go (www.mp3erger.ru) 2023.mp3", 
+        "music/nemra_-_last_chance_to_love_(z3.fm).mp3",
+        "music/Мот & MeMaria - Hotel Rendezvous (1).mp3",
+        "music/2e849b6a74592e9a96506acec6b3587c_nemra-daisy.mp3",
+        "music/Nemra - Nare (Original Instrumental, Karaoke, Minus) 2020.mp3",
+    ],
+    poster :["https://i.ytimg.com/vi/8Gy2ZDGLteQ/sddefault.jpg",
+             "https://i1.sndcdn.com/artworks-000133937425-n6zixi-t500x500.jpg",
+             "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSF2glJgJADyP3u6nFHWJ8kbkonr268EA-aF-OaG4Hyyw&s",
+             "https://images.genius.com/62d7ead5a96680000bb8458179f4ca55.592x592x1.png",
+             "https://i.ytimg.com/vi/nwdGPR_JPao/maxresdefault.jpg",
+]
+}
